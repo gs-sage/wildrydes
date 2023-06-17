c6a1fb95ba28b70509686fac0306b414cf336865
gs-sage <gauravsaymiaws@gmail.com>
2023-06-17 17:37:09 -0400

    new_config

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-east-1_Jdfu08DUV', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '4l5c8gm7t4v739s77g902cm89q', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
